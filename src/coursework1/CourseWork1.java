ba1c69c09cddfe87263f8c546b1e8600dd5ef10f
dj24 <dj24@users.noreply.github.com>
2017-12-05 10:18:36 +0000

    Code Condense
    removed extra class for searching functions, adding it to the main class to make the program more akin to my original psudeo code for the algorithm

--- src/coursework1/CourseWork1.java
+++ src/coursework1/CourseWork1.java
@@ -13,18 +13,61 @@ import java.util.Arrays;
  */
 public class CourseWork1 {
     
-    public static void main(String[] args) 
+    static void run(double[] S, double[][] T){
+        
+        int k = S.length;
+        double[] Q = new double[k];
+        
+        double distance = 0;
+        int[] index = {0,0};
+        for(int i = 0; i<T.length;i++)
+        {
+            for(int j = 0; j<T[i].length-k+1;j++)
+            {
+                Q = Arrays.copyOfRange(T[i], j, j+k);
+                
+                double sum = 0;
+                for(int l = 0; l < S.length; l++)
+                    sum+=(S[l]-Q[l])*(S[l]-Q[l]);
+                sum = Math.sqrt(sum);
+                
+                if(sum < distance || (j == 0 && i == 0))
+                {
+                    index[0] = i;
+                    index[1] = j;
+                    distance = sum;
+                    System.out.println(distance);
+                }
+            }
+        }
+    }
+    
+    static public void experiment(int n)
     {
         RandomGenerator T = new RandomGenerator();
-        
-        SubseriesSearch sub = new SubseriesSearch();
-        
-        
-        
-        for(int i = 0; i <= 500; i+=20)
+        run(T.makeQuery(3), T.makeSet(n));
+    }
+    
+    static public void timingExperiment(int n, int reps)
+    {
+        double sum = 0;
+        for(int i = 0; i< reps; i++) 
         {
-            sub.timingExperiment(i,1000);
+            long t1 = System.nanoTime();
+            experiment(n);
+            sum+= (System.nanoTime() - t1)/10000000.0; 
+            //System.out.println(sum);
         }
+        if(n != 0)
+            System.out.println(sum/100);
+    }
+    public static void main(String[] args) 
+    {
+        experiment(50);
+        
+        
+        
+        
     }
     
 }
